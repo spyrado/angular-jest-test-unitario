7e6b4ef54774e5fdea7b327edcbda5c89b96b310
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2022-01-30 14:44:21 -0300

    removendo espaços

--- src/app/pages/posts/posts.component.spec.ts
+++ src/app/pages/posts/posts.component.spec.ts
@@ -61,28 +61,20 @@ describe('PostsComponent', () => {
     }));
 
     it('BEFORE execute funcion posts.length should be equal to 0', fakeAsync (() => {
-      
       expect(component.posts.length).toEqual(0);
       component.getPosts();
-
       tick();
     }));
 
     it('AFTER execute funcion posts.length should be equal to 1', fakeAsync (() => {
-
       component.getPosts();
-
       tick();
-
       expect(component.posts.length).toEqual(1);
     }));
 
     it('should receive posts from service and NOT transform the array.', fakeAsync (() => {
-
       component.getPosts();
-
       tick();
-      
       expect(component.posts).toEqual(GET_POSTS);
     }));
   });
